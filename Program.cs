e82b91c6dbabe240f409ebac142bb46effd99eb6
emolesky <emilynnmolesky@gmail.com>
2025-02-06 11:25:53 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
-﻿namespace WatchTower
+﻿//Emilynn Molesky
+//1/5/2025
+
+
+
+namespace MagicCannon
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            EnemyWatch aEnemyWatch = new EnemyWatch(2, 3);
-            string location = aEnemyWatch.CalcDirection();
-            Console.WriteLine(location);
+            for (int i = 1; i <= 100; i++)
+            {
+                bool fire = (i % 3 == 0);
+                bool electric = (i % 5 == 0);
+
+                if (fire && electric)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{i}: Fire and Electric");
+                }
+                if (fire)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{i}: Fire");
+                }
+                if (electric)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{i}:Electric");
+                }
+                else
+                {
+                    Console.ResetColor();
+                    Console.WriteLine($"{i}: Normal");
+                }
+            }
         }
     }
 }
