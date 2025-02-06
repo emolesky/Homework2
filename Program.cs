48b764b0984390a69c4ec07812f8efb78a206603
emolesky <emilynnmolesky@gmail.com>
2025-02-06 11:24:02 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,64 @@
-﻿//Emilynn Molesky
-//2/5/2025
+﻿//Emilynn Molesky 
+//1/5/2025
 
-namespace Replicator
+
+using System.ComponentModel.Design;
+
+namespace ConsoleApp1
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            int[] originalArray = new int[5];
+           
 
-            Console.WriteLine("Enter 5 Numbers:");
-            for (int i = 0; i < originalArray.Length; i++)
+           
+
+            Console.WriteLine("The follwing items are available:");
+            Console.WriteLine("1- Rope");
+            Console.WriteLine("2- Torches");
+            Console.WriteLine("3- Climbing Equipment");
+            Console.WriteLine("4- Clean Water");
+            Console.WriteLine("5- Machete");
+            Console.WriteLine("6- Canoe");
+            Console.WriteLine("7- Food Supplies");
+            Console.WriteLine("What number do you want to see the cost of?");
+            
+
+            int itemNumber = Convert.ToInt32(Console.ReadLine());
+
+            string item = itemNumber switch
             {
-                Console.WriteLine($"Number {i + 1}:");
-                originalArray[i] = int.Parse(Console.ReadLine());
-            }
+                1 => "Rope",
+                2 => "Torches",
+                3 => "Climbing Eqiupment",
+                4 => "Clean Water",
+                5 => "Machete",
+                6 => "Canoe",
+                7 => "Food Supplies"
+            };
 
-            int[] copyArray = new int[5];
-
-            for (int i = 0; i < originalArray.Length; i++)
+            int price = item switch
             {
-                copyArray[i] = originalArray[i];
-            }
+                "Rope" => 10,
+                "Torches" => 15,
+                "Climbing Equipment" => 25,
+                "Clean Water" => 1,
+                "Machete" => 20,
+                "Canoe" => 2,
+                "Food Supplies" => 1
 
-            Console.WriteLine("Original Array:" + string.Join(", ", originalArray));
-            Console.WriteLine("Copy Array:" + string.Join(", ", copyArray));
+            };
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            if (name == "Chloe") price /= 2;
+            Console.WriteLine($"{item} costs {price} gold.");
+            
+            
+     
+
 
         }
     }
