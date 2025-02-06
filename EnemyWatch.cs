d54ccaa6ec68d7a956959547c132394461b5bca6
emolesky <emilynnmolesky@gmail.com>
2025-02-06 11:22:42 -0600

    Add files via upload

---
+++ EnemyWatch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace WatchTower
+{
+    public class EnemyWatch
+    {
+        private int x;
+        private int y;
+
+
+        public int X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+        public int Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+        public EnemyWatch(int aX, int aY)
+        {
+            this.x = aX;
+            this.y = aY;
+        }
+        public string CalcDirection()
+        {
+            {
+                if (x == 0 && y == 0)
+                    return "The enemy is here!";
+                if (x == 0 && y < 0)
+                    return "The enemy is South!";
+                if (x == 0 && y > 0)
+                    return "The enemy is North!";
+                if (x < 0 && y > 0)
+                    return "The enemy is North West!";
+                if (x < 0 && y == 0)
+                    return "The enemy is West!";
+                if (x < 0 && y < 0)
+                    return "The enemy is South West!";
+                if (x > 0 && y > 0)
+                    return "The enemy is North East!";
+                if (x > 0 && y == 0)
+                    return "The enemy is East!";
+                if (x > 0 && y < 0)
+                    return "The enemy is South East!";
+                return "Unknown Message";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Emilynn Molesky
+//2/5/2025
+
+namespace Replicator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] originalArray = new int[5];
+
+            Console.WriteLine("Enter 5 Numbers:");
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                Console.WriteLine($"Number {i + 1}:");
+                originalArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            int[] copyArray = new int[5];
+
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                copyArray[i] = originalArray[i];
+            }
+
+            Console.WriteLine("Original Array:" + string.Join(", ", originalArray));
+            Console.WriteLine("Copy Array:" + string.Join(", ", copyArray));
+
+        }
+    }
+}
